4ab53b2eb8f0b2ba37bdc30457f5475e122d6872
Pandora <mitkiril@gmail.com>
2019-12-22 21:55:32 +0300

    exceptions

--- src/CustomerStorage.java
+++ src/CustomerStorage.java
@@ -1,33 +1,31 @@
 import java.util.HashMap;
 
-public class CustomerStorage
-{
+public class CustomerStorage {
     private HashMap<String, Customer> storage;
 
-    public CustomerStorage()
-    {
+    public CustomerStorage() {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data)
-    {
+    public void addCustomer(String data) {
         String[] components = data.split("\\s+");
         String name = components[0] + " " + components[1];
-        storage.put(name, new Customer(name, components[3], components[2]));
+//        String email = String.valueOf(components[3].matches("(.+@.+\\..+)"));
+//        String number = String.valueOf(components[2].matches("\\+7\\d{10}"));
+        if (components[3].matches("\\+7\\d{10}") & components[2].matches("(.+@.+\\..+)")) {
+            storage.put(name, new Customer(name, components[3], components[2]));
+        }else System.out.println("invalid number or e-mail format");
     }
 
-    public void listCustomers()
-    {
+    public void listCustomers() {
         storage.values().forEach(System.out::println);
     }
 
-    public void removeCustomer(String name)
-    {
+    public void removeCustomer(String name) {
         storage.remove(name);
     }
 
-    public int getCount()
-    {
+    public int getCount() {
         return storage.size();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
-public class Main
-{
+/**
+ * Прописать все возможные защиты от преждевременного завершения программы и от некорректной работы с помощью
+ * Exception-ов - неверного формата команды, неверного номера телефона и неверного e-mail'а.
+ */
+public class Main {
     private static String addCommand = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static String commandExamples = "\t" + addCommand + "\n" +
@@ -10,31 +13,27 @@ public class Main
             commandExamples;
     private static String helpText = "Command examples:\n" + commandExamples;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
-        for(;;)
-        {
+        for (; ; ) {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
-            if(tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("list")) {
+            if (tokens[0].equals("add")) {
+                try {
+                    executor.addCustomer(tokens[1]);
+                }catch(ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Invalid data format. Example: " + addCommand);
+                }
+            } else if (tokens[0].equals("list")) {
                 executor.listCustomers();
-            }
-            else if(tokens[0].equals("remove"))
-            {
+            } else if (tokens[0].equals("remove")) {
                 executor.removeCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("count")) {
+            } else if (tokens[0].equals("count")) {
                 System.out.println("There are " + executor.getCount() + " customers");
-            }
-            else if(tokens[0].equals("help")) {
+            } else if (tokens[0].equals("help")) {
                 System.out.println(helpText);
-            }
-            else {
+            } else {
                 System.out.println(commandError);
             }
         }
